4372365c5f7010f4b59a43fc18e76e6668b90bb1
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-05-10 14:55:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -231,8 +231,8 @@ with tabs[1]:
         font-weight: bold;
         border-radius: 12px;
         border: none;
-        width: 100%;
-        height: 110px;
+        width: 130%;
+        height: 70px;
         margin-bottom: 20px;
         transition: 0.3s;
     }
