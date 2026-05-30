ee75679ed6931f8ac0be53f6091c4f9b4dda624d
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-05-30 10:14:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -539,8 +539,11 @@ with col3:
 # ======================================
 # LOGIN STATE
 # ======================================
-if "show_login" not in st.session_state:
-    st.session_state.show_login = False
+if (
+    st.session_state.show_login
+    and st.session_state.auth_mode == "login"
+    and not st.session_state.get("logged_in", False)
+):
 
 # OPEN LOGIN PAGE
 if login_btn:
